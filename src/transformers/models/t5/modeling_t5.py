9eef26e8dfe4c2a957388903e366c6a1eb42e714
yoel shoshan <yoels@il.ibm.com>
2023-11-20 00:21:30 -0500

    ...

--- src/transformers/models/t5/modeling_t5.py
+++ src/transformers/models/t5/modeling_t5.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """ PyTorch T5 model."""
-
+assert False, 'development moved to branch "flash"'
 
 import copy
 import math
